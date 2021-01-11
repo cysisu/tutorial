277202d6f7408e9f0b111446203ed75fbed6b184
cglxynpy <2867800498@qq.com>
2021-01-12 01:00:06 +0800

    upload some images

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tutorial
+![img_1.png](img_1.png)
+![img_2.png](img_2.png)
+![img_3.png](img_3.png)
--- src/main/java/com/tutorial/cn/ListTest.java
+++ src/main/java/com/tutorial/cn/ListTest.java
@@ -1,27 +1,15 @@
 package com.tutorial.cn;
 
+import javax.print.attribute.standard.PrinterResolution;
 import javax.xml.soap.Node;
 import java.util.*;
 
 public class ListTest {
     public static void main(String[] args) {
-        Map<Line,Integer> map=new TreeMap<>();
-
+        int[][] points={{2,3},{3,3},{-5,3}};
+        Solution solution=new Solution();
+        System.out.println(solution.maxPoints(points));
     }
-
-    public int maxPoints(int[][] points) {
-        if(points.length==0){
-            return 0;
-        }
-        Map<Line,Integer> map=new TreeMap<>();
-        for(int i=0;i<points.length;i++){
-            for(int j=i+1;j<points.length;j++){
-
-            }
-        }
-    }
-
-
 }
 
 
